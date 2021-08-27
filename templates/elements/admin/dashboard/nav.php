d6babebe49da8df0d88a63663c6ea3512c132d4c
Nur Ikhwan Idris <35907238+nurikhwanidris@users.noreply.github.com>
2021-08-27 16:50:28 +0800

    asd

--- templates/elements/admin/dashboard/nav.php
+++ templates/elements/admin/dashboard/nav.php
@@ -224,6 +224,22 @@ $rowLeave = mysqli_fetch_array($resultLeave);
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
 
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseReport" aria-expanded="true" aria-controls="collapseReport">
+                    <i class="fas fa-table"></i>
+                    <span>Report</span>
+                </a>
+                <div id="collapseReport" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Report</h6>
+                        <a class="collapse-item" href="/project/templates/homedecor/report/generate">Generate</a>
+                    </div>
+                </div>
+            </li>
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
---
+++ templates/homedecor/report/generate.php
@@ -0,0 +1,37 @@
+<!-- Title -->
+<?php $title = 'Sales Dashboard'; ?>
+
+<!-- Get DB conn -->
+<?php include('../../../src/model/dbconn.php') ?>
+
+<!-- Header -->
+<?php include('../../elements/admin/dashboard/header.php') ?>
+
+<!-- Datatable CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/rowgroup/1.1.3/css/rowGroup.dataTables.min.css">
+
+<!-- Sidebar -->
+<?php include('../../elements/admin/dashboard/nav.php') ?>
+
+
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Customer Sales Dashboard</h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 col-xl-12">
+            <div class="card shadow mb-4">
+                <div class="card-header">
+                    asdasd
+                </div>
+                <div class="card-body">
+                    asd
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
